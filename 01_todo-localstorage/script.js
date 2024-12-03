c2d3ec1bc5f0d009adea4efc2b4c5849e6e655fa
princegupta-in <zprincegupta@gmail.com>
2024-12-03 22:25:56 +0530

    completed

--- 01_todo-localstorage/script.js
+++ 01_todo-localstorage/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const todoInput = document.getElementById('todo-input')
   const todoList = document.getElementById('todo-list')
 
-  const arrayOfTasks = JSON.parse(localStorage.getItem('keyName')) || [];
+  let arrayOfTasks = JSON.parse(localStorage.getItem('keyName')) || [];
 
   arrayOfTasks.forEach(element => {
     renderTasks(element);
@@ -20,17 +20,42 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     arrayOfTasks.push(object);
     saveTaskInLocalStorage();
+    renderTasks(object)
     todoInput.value = ""; //clear input
     console.log(arrayOfTasks);
 
   })
 
-  function renderTasks(kuchVNaam){
-    console.log(kuchVNaam);
+  function renderTasks(kuchVNaam) {
+    // console.log(kuchVNaam);
+    const li = document.createElement('li')
+    li.setAttribute('data-id', kuchVNaam.id)
+    li.innerHTML = `<span>${kuchVNaam.text}</span> <button>delete</button>`
+    if(kuchVNaam.completed) li.classList.add('completed')//this will check if byDefault completed true hai then apne aap completed naam ka class li me lag jayega and ushka css apply ho jayega
+
+    li.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') return
+
+      kuchVNaam.completed = !kuchVNaam.completed //completed ko toggle kro
+      // console.log(kuchVNaam.completed);
+      li.classList.toggle('completed')
+      saveTaskInLocalStorage()
+    })
+
+    li.querySelector('button').addEventListener('click',(e)=>{
+      e.stopPropagation()
+      arrayOfTasks = arrayOfTasks.filter((t)=>(t.id !== kuchVNaam.id))
+      li.remove()
+      saveTaskInLocalStorage()
+    })
+
+    todoList.appendChild(li)
+    console.log(arrayOfTasks);
+    
     
   }
 
-  function saveTaskInLocalStorage(){
-    localStorage.setItem('keyName',JSON.stringify(arrayOfTasks));
+  function saveTaskInLocalStorage() {
+    localStorage.setItem('keyName', JSON.stringify(arrayOfTasks));
   }
 })
